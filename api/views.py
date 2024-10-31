fb8cd65413e9c8a0b33330a764230936f2099c26
Abbosjon Absalomov <105669161+ABBOSXAN@users.noreply.github.com>
2024-10-31 10:52:50 +0500

    api updated

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .serializers import PostSerializer
+from posts.models import Post
+from rest_framework import generics
 # Create your views here.
+
+class PostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetailAPIView(generics.RetrieveUpdateDestroyAPIView): # detail, update, delete
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
